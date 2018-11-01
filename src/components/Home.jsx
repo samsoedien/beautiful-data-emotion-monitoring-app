1e649699f0fbe61d50f8845c6855aa551efbec6f
Samsoedien <samsoedien@wlan-200120.nbw.tue.nl>
2018-11-01 11:58:25 +0100

    instructions update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,18 +6,26 @@ const Home = () => {
       <h1>Instructions</h1>
       <div className="container">
         <p className="lead">
-          This is a data collector and visualisation for stress related to heart
-          data. In the User form irregular BPM values are filter and the user
-          can fill in some contextual data to enrich the dataset.
+          This is a data collector and visualisation for the heartbeat
+          wristband. In the Self-assessment form contextual stressdata can be
+          submitted to the database.
         </p>
         <p className="">
-          The application is build in react and is connected to a Rest API. Go
+          The application is build in react and is connected to a REST API. Go
           to{" "}
           <a href="https://beautiful-data.herokuapp.com/api/users/1">
             https://beautiful-data.herokuapp.com/api/users/1
           </a>{" "}
-          to view heartdata. Post request can be send to this end-point (bpm:
-          Number).
+        </p>
+        <br />
+        <p>
+          To view the four datasets of all information products. Post requests
+          can be send to the endpoints /weatherdata, /drowsinessdata,
+          /stressdata and /heartdata. See{" "}
+          <a href="https://github.com/samsoedien/ESP8266-data-collector">
+            https://github.com/samsoedien/ESP8266-data-collector
+          </a>{" "}
+          for instructions how to configure data collectors for the REST API.
         </p>
       </div>
     </div>
